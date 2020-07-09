4cd917ecefbf1432bed9fd90575a991193fafa8f
Punkwalker <39736560+Punkwalker@users.noreply.github.com>
2020-07-08 19:55:46 -0500

    FINAL

--- Project/myDBupdate.sql
+++ Project/myDBupdate.sql
@@ -1,20 +1,20 @@
 
---Update querry to change names of holiday special trains
+--Update querry to change names of holiday trains
 
 
 UPDATE s20_1_train
-SET train_name = 'PUNE-MUMBAI-HOLIDAY-SPECIAL'
-WHERE train_no = 11121
+	SET train_name = 'PUNE-MUMBAI-HOLIDAY-SPECIAL'
+	WHERE train_no = 11121;
 
 UPDATE s20_1_train
-SET train_name = 'MUMBAI-PUNE-HOLIDAY-SPECIAL'
-WHERE train_no = 11122
+	SET train_name = 'MUMBAI-PUNE-HOLIDAY-SPECIAL'
+	WHERE train_no = 11122;
 
 UPDATE s20_1_train
-SET train_name = 'DELHI-JAIPUR-HOLIDAY-SPECIAL'
-WHERE train_no = 11123
+	SET train_name = 'DELHI-JAIPUR-HOLIDAY-SPECIAL'
+	WHERE train_no = 11123;
 
 UPDATE s20_1_train
-SET train_name = 'JAIPUR-DELHI-HOLIDAY-SPECIAL'
-WHERE train_no = 11124
+	SET train_name = 'JAIPUR-DELHI-HOLIDAY-SPECIAL'
+	WHERE train_no = 11124;
 
